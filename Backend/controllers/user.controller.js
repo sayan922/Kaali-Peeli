730d75b23583b52e2daaf669f5ce8b1d027ecb4d
sayanophile <chakrabortysayan922@gmail.com>
2025-06-25 19:35:07 +0530

    replace

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-const blackListTokenModel = require('../models/blackListToken.model');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
 
@@ -72,7 +72,7 @@ module.exports.logoutUser = async (req, res, next) => {
     res.clearCookie('token');
     const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
 
-    await blackListTokenModel.create({ token });
+    await blacklistTokenModel.create({ token });
 
     res.status(200).json({ message: 'Logged out' });
 
